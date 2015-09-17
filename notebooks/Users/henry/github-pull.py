72de5887fc2647cdc1112f8b292a2cd76607e76f
Henry Davidge <hhd@databricks.com>
2015-09-17 16:27:57 -0700

    Update github-pull.py

--- notebooks/Users/henry/github-pull.py
+++ notebooks/Users/henry/github-pull.py
@@ -1,6 +1,14 @@
-# Databricks notebook source exported at Thu, 17 Sep 2015 23:26:45 UTC
+# Databricks notebook source exported at Thu, 17 Sep 2015 23:27:57 UTC
 1 + 1
 
 # COMMAND ----------
 
 sc.parallelize(range(1, 100)).count() + 1
+
+# COMMAND ----------
+
+2 + 2
+
+# COMMAND ----------
+
+3 + 3
