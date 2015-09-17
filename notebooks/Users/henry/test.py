f77e833991a65e14ed8ecc43bfa19a0c7325c7d4
Henry Davidge <hhd@databricks.com>
2015-09-17 15:56:31 -0700

    Update test.py

--- notebooks/Users/henry/test.py
+++ notebooks/Users/henry/test.py
@@ -1,6 +1,6 @@
-# Databricks notebook source exported at Thu, 17 Sep 2015 02:40:01 UTC
+# Databricks notebook source exported at Thu, 17 Sep 2015 22:56:31 UTC
 1 + 1
 
 # COMMAND ----------
 
-sc.parallelize(range(1, 100)).count()
+sc.parallelize(range(1, 100)).count() + 1
